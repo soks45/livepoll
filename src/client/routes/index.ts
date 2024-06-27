24aa9e76b3a52c8b5cb7c87ca7b0519de19a1c05
Aleksey Batuev <n.batuev@infinnity.ru>
2024-06-27 23:13:42 +0500

    hello world client

--- src/client/routes/index.ts
+++ src/client/routes/index.ts
@@ -1,5 +1,8 @@
 import express, { Router } from 'express';
 
 const clientRouter: Router = express.Router();
+clientRouter.get('/', (req: express.Request, res: express.Response) => {
+    res.render('page');
+})
 
 export { clientRouter };
