054a1999ad8cc20f2eaa30141f5f5668abb5d617
Aleksey Batuev <n.batuev@infinnity.ru>
2024-06-27 22:50:11 +0500

    database service

--- src/api/services/database.service.ts
+++ src/api/services/database.service.ts
@@ -1,7 +1,20 @@
-class DatabaseService {
+import { Client } from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+class DatabaseService extends Client {
+	constructor() {
+		super({
+			user: process.env.DATABASE_USER,
+			database: process.env.DATABASE_NAME,
+			host: process.env.DATABASE_HOST,
+			port: Number(process.env.DATABASE_PORT),
+			password: process.env.DATABASE_PASSWORD,
+		})
+	}
 }
 
-const instance: DatabaseService = new DatabaseService();
+const instance = new DatabaseService();
 
 export { instance as DatabaseService };
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
 import express, { Express } from 'express';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
+import * as process from 'node:process';
 import { setupApi } from './api';
+import { DatabaseService } from './api/services/database.service';
 import { setupClient } from './client';
 import { errorHandler } from './midlewares/error.handler';
+import dotenv from 'dotenv';
 
-const app: Express = express();
+async function bootstrap(): Promise<void> {
+    dotenv.config();
+    const app: Express = express();
+    app.use(logger('dev'))
+        .use(express.json())
+        .use(express.urlencoded({ extended: false }))
+        .use(cookieParser());
+    setupClient(app);
+    setupApi(app);
+    app.use(errorHandler);
 
-app.use(logger('dev'))
-    .use(express.json())
-    .use(express.urlencoded({ extended: false }))
-    .use(cookieParser());
-setupClient(app);
-setupApi(app);
-app.use(errorHandler);
+    await DatabaseService.connect();
 
-const port: number = 3000;
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+    const port: number = Number(process.env.SERVER_PORT);
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
+}
+
+bootstrap();
