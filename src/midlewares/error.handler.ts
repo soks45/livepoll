73507719ac2132f247d105a64ff0fe20877f1d73
Aleksey Batuev <n.batuev@infinnity.ru>
2024-06-28 23:30:50 +0500

    app structure

--- src/midlewares/error.handler.ts
+++ src/midlewares/error.handler.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction): void => {
-    console.log(123);
-    console.error(err.stack);
     res.status(500).send('Something broke!');
 };
